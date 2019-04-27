6437f03b28c674c3fbc7f7d32dde7f735cbfea7c
张志勇 <515357583@qq.com>
2019-04-27 16:39:42 +0800

    格式化些许文档

---
+++ pages/home.js
@@ -0,0 +1,33 @@
+
+import Router from 'next/router'
+import Head from 'next/head'
+import Link from 'next/link'
+import { connect } from 'react-redux'
+import { Button } from 'antd'
+
+class home extends React.Component {
+  render() {
+    return <div style={{ height: '300px', width: '1024px', margin: '100px auto', fontSize: '30px' }}
+      onClick={() => {
+        Router.push({ pathname: '/index' })
+      }}
+    >
+      <Head>
+        <title>Home页面</title>
+      </Head>
+      <span style={{ color: this.props.testReducer.color }}>Home</span>
+      <Link href={{ pathname: '/index' }}>
+        <Button type="danger" size="large">返回到index页</Button>
+      </Link>
+    </div>
+  }
+}
+
+const mappStateToProps = (state) => {
+  return {
+    ...state
+  }
+}
+
+
+export default connect(mappStateToProps, null)(home)
