35610e44be73a2238d4ddb18a507fa85f6350ba0
张志勇 <515357583@qq.com>
2019-06-04 17:12:14 +0800

    添加课程页面

--- kits/fetchHelper.js
+++ kits/fetchHelper.js
@@ -6,6 +6,8 @@ function fetchHelper(urlPath,options){
   return fetch(baseUrl+urlPath,options)
 }
 
+fetchHelper.url = baseUrl;
+
 // 2.0 封装好get请求
 fetchHelper.get = function(urlPath) {
   return fetch(baseUrl+urlPath,{
